ffc7a4f190cadc19294ed5de891321e8d180075e
SolomonChidera <solochidera10@gmail.com>
2023-09-25 14:51:06 +0100

    Creating a single linked list

--- LinkedList/SinglyLinkedList/first.c
+++ LinkedList/SinglyLinkedList/first.c
@@ -1,1 +1,19 @@
 #include "dsa.h"
+
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+int main(void)
+{
+	struct node *head;
+	head = malloc(sizeof(struct node));
+
+	head->data = 100;
+	head->next = NULL;
+	printf("%d\n", head->data);
+	return (0);
+}
