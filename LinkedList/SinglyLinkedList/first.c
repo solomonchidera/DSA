8157dbd7935dfa353b20983df849b70047bd6155
SolomonChidera <solochidera10@gmail.com>
2023-09-25 14:57:39 +0100

    Creating a single linked list/adding another node

--- LinkedList/SinglyLinkedList/first.c
+++ LinkedList/SinglyLinkedList/first.c
@@ -9,11 +9,18 @@ struct node
 
 int main(void)
 {
-	struct node *head;
+	struct node *head, newnode;
 	head = malloc(sizeof(struct node));
 
 	head->data = 100;
 	head->next = NULL;
 	printf("%d\n", head->data);
+
+	newnode = malloc(sizeof(struct node));
+	newnode->data = 102;
+	newnode->next = NULL;
+	head->next = newnode;
+	printf("%d\n", newnode->data);
+
 	return (0);
 }
