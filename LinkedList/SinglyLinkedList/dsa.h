54fd77c6d566d2cc718ed872f0c64b90408e11a6
SolomonChidera <solochidera10@gmail.com>
2023-09-25 16:50:26 +0100

    Header file with a struct[node]

--- LinkedList/SinglyLinkedList/dsa.h
+++ LinkedList/SinglyLinkedList/dsa.h
@@ -1,8 +1,24 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef DSA_H
+#define DSA_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
 #endif
