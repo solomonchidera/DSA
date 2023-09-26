eb46f650be1e96e0d850f24427847b310e521e23
SolomonChidera <solochidera10@gmail.com>
2023-09-26 14:43:09 +0100

    Looks like am confused here ::

--- LinkedList/SinglyLinkedList/second.c
+++ LinkedList/SinglyLinkedList/second.c
@@ -6,17 +6,27 @@ typedef struct node
 	struct node *next;
 } node;
 
-node *head, newnode, temp;
-head = NULL;
+void print();
 
-newnode = malloc(sizeof(node));
-newnode->data = 19;
-newnode->next = NULL;
-head->next = newnode;
-newnode = malloc(sizeof(node));
-newnode->data = 30;
-newnode->next = NULL;
-newnode->next = newnode;
+int main(void)
+{
+
+
+	node *head, *newnode, *temp;
+	head = newnode;
+
+	newnode = malloc(sizeof(node));
+	newnode->data = 19;
+	newnode->next = NULL;
+	head->next = newnode;
+	newnode = malloc(sizeof(node));
+	newnode->data = 30;
+	newnode->next = NULL;
+	newnode->next = newnode;
+	
+	print();
+	return (0);
+}
 
 void print()
 {
@@ -27,10 +37,4 @@ void print()
 		printf("%d ", temp->data);
 	}
 	printf("\n");
-}
-
-int main(void)
-{
-	print();
-	return (0);
 }
