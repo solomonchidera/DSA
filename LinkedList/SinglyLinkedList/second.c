ce96a30d6587a9e18e3ae548891e6753a3355064
SolomonChidera <solochidera10@gmail.com>
2023-09-26 14:35:23 +0100

    Looks like am confused here ::

---
+++ LinkedList/SinglyLinkedList/second.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+typedef struct node
+{
+	int data;
+	struct node *next;
+} node;
+
+node *head, newnode, temp;
+head = NULL;
+
+newnode = malloc(sizeof(node));
+newnode->data = 19;
+newnode->next = NULL;
+head->next = newnode;
+newnode = malloc(sizeof(node));
+newnode->data = 30;
+newnode->next = NULL;
+newnode->next = newnode;
+
+void print()
+{
+	temp = head;
+
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	print();
+	return (0);
+}
