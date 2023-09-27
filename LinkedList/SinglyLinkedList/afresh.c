08012fc8ef626b8a6c2f4ab5e1f649a59207c8dc
SolomonChidera <solochidera10@gmail.com>
2023-09-27 14:34:11 +0100

    Inserting a node

---
+++ LinkedList/SinglyLinkedList/afresh.c
@@ -0,0 +1,30 @@
+#include "das.h"
+
+struct Node
+{
+	int data;
+	struct Node *next;
+};
+
+int main(void)
+{
+
+
+	struct Node *head = NULL;
+	struct Node *current;
+	struct Node *newnode;
+	newnode = malloc(sizeof(struct node));
+	newnode->data = 48;
+	newnode->next = NULL;
+
+	newnode->next = head;
+	head = newnode;
+	current = head;
+
+	while (current != NULL)
+	{
+		printf("The content of the node is %d\n", current->data);
+		current = current->next;
+	}
+	return (0);
+}
